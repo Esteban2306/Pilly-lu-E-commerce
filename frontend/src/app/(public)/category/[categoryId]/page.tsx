a831fc59a0d6b368a7c472c8221f67ca9af31b88
esteban_castañeda <esteban2306dd@gmail.com>
2025-10-17 18:24:35 -0500

    better ui of category and products

--- frontend/src/app/(public)/category/[categoryId]/page.tsx
+++ frontend/src/app/(public)/category/[categoryId]/page.tsx
@@ -1,44 +1,82 @@
+import Image from "next/image"
 import { productApi } from "@/services/ProductApi"
 import ProductGallery from "@/components/product/productGallery/productGallery"
-import { Category, Product } from "@/types/productsCategory.types";
+import { Category, Product } from "@/types/productsCategory.types"
+
+import aretes from "../../../../../public/icons/iconAirrings.svg"
+import manilla from "../../../../../public/icons/iconBracelet.svg"
+import cadenas from "../../../../../public/icons/iconChains.svg"
+import dijes from "../../../../../public/icons/iconCharms.svg"
+import anillos from "../../../../../public/icons/iconRing.svg"
+
 
 
 export default async function productIdPage({ params }: { params: { categoryId: string } }) {
+    const { category, products } = await productApi.getByCategory<{ category: Category; products: Product[] }>(
+        params.categoryId
+    )
 
-    const { category, products } = await productApi.getByCategory<{ category: Category; products: Product[] }>(params.categoryId)
+    const iconMap: Record<string, any> = {
+        anillos,
+        aretes,
+        manillas: manilla,
+        dijes,
+        cadenas,
+    }
+
+    const iconoCategoria = iconMap[category.categoryName.toLowerCase()] || anillos
+
 
     if (!products.length) {
         return (
-            <main className="min-h-screen flex flex-col items-center justify-center">
-                <h1 className="text-3xl font-bold">Categoría vacía</h1>
-                <p>No hay productos en esta categoría todavía.</p>
+            <main className="min-h-screen flex flex-col items-center justify-center text-center">
+                <h1 className="text-3xl font-bold text-zinc-800">Categoría vacía</h1>
+                <p className="text-zinc-500 mt-2">No hay productos en esta categoría todavía.</p>
             </main>
-        );
+        )
     }
 
-    return (<main className='min-h-screen'>
-        <div className="flex justify-center mt-28">
-            <h1 className="text-4xl font-medium mb-4 h-c">{category.categoryName}</h1>
-        </div>
-        <div className="
+    return (
+        <main className="min-h-screen pb-20 ">
+            <div className="flex flex-col items-center mt-32 mb-10">
+                <div className="group relative w-28 h-28 rounded-full bg-gradient-to-br from-blue-200 to-blue-100 flex items-center justify-center shadow-md hover:shadow-xl hover:-translate-y-2 transition-all duration-300">
+                    <Image
+                        src={iconoCategoria}
+                        alt={category.categoryName}
+                        width={60}
+                        height={60}
+                        className="object-contain group-hover:scale-110 transition-transform duration-300"
+                    />
+                </div>
+
+                <h1 className="mt-6 text-5xl font-extrabold  bg-clip-text bg-gradient-to-r text-black tracking-wide uppercase drop-shadow-sm">
+                    {category.categoryName}
+                </h1>
+
+            </div>
+            <div
+                className="
                     grid 
                     justify-center 
-                    gap-4 
+                    gap-7 
                     mt-20 mb-18
                     [grid-template-columns:repeat(1,280px)]
                     sm:[grid-template-columns:repeat(2,280px)]
                     lg:[grid-template-columns:repeat(3,280px)]
-                ">
-            {products.map((p) => (
-                <ProductGallery
-                    key={p._id}
-                    _id={p._id}
-                    productName={p.productName}
-                    price={p.price}
-                    images={p.images}
-                />
-            ))}
-        </div>
-    </main>
+        "
+            >
+                {products.map((p) => (
+                    <ProductGallery
+                        key={p._id}
+                        _id={p._id}
+                        productName={p.productName}
+                        price={p.price}
+                        images={p.images}
+                        color={p.color}
+                        offer={p.offer}
+                    />
+                ))}
+            </div>
+        </main>
     )
 }
--- frontend/src/app/(public)/products/page.tsx
+++ frontend/src/app/(public)/products/page.tsx
@@ -1,73 +1,88 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import aretes from '../../../../public/icons/iconAirrings.svg';
-import manilla from '../../../../public/icons/iconBracelet.svg';
-import cadenas from '../../../../public/icons/iconChains.svg';
-import dijes from '../../../../public/icons/iconCharms.svg';
-import anillos from '../../../../public/icons/iconRing.svg';
-import ProductGallery from '@/components/product/productGallery/productGallery';
-import { productApi } from '@/services/ProductApi';
-import { Product } from '@/types/productsCategory.types';
+import Image from "next/image";
+import Link from "next/link";
+import aretes from "../../../../public/icons/iconAirrings.svg";
+import manilla from "../../../../public/icons/iconBracelet.svg";
+import cadenas from "../../../../public/icons/iconChains.svg";
+import dijes from "../../../../public/icons/iconCharms.svg";
+import anillos from "../../../../public/icons/iconRing.svg";
+import ProductGallery from "@/components/product/productGallery/productGallery";
+import { productApi } from "@/services/ProductApi";
+import { Product } from "@/types/productsCategory.types";
 
 export default async function productsHome() {
-
-    type CategoriasType = {
-        nombre: string;
-        icono: string;
-        slug: string;
-    };
-
     const categorias = [
-        { nombre: "Anillos", icono: anillos, slug: "anillos", categoryId: '68c841d9aa6f123a0efb5c80' },
-        { nombre: "Aretes", icono: aretes, slug: "aretes", categoryId: '68c841bfaa6f123a0efb5c7e' },
-        { nombre: "Manilla", icono: manilla, slug: "manillas", categoryId: '68c841ecaa6f123a0efb5c82' },
-        { nombre: "Dijes", icono: dijes, slug: "dijes", categoryId: '68c841f1aa6f123a0efb5c84' },
-        { nombre: "Cadenas", icono: cadenas, slug: "cadenas", categoryId: '68c841fbaa6f123a0efb5c86' },
+        { nombre: "Anillos", icono: anillos, slug: "anillos", categoryId: "68c841d9aa6f123a0efb5c80" },
+        { nombre: "Aretes", icono: aretes, slug: "aretes", categoryId: "68c841bfaa6f123a0efb5c7e" },
+        { nombre: "Manilla", icono: manilla, slug: "manillas", categoryId: "68c841ecaa6f123a0efb5c82" },
+        { nombre: "Dijes", icono: dijes, slug: "dijes", categoryId: "68c841f1aa6f123a0efb5c84" },
+        { nombre: "Cadenas", icono: cadenas, slug: "cadenas", categoryId: "68c841fbaa6f123a0efb5c86" },
     ];
 
-    const product = await productApi.getAll<Product[]>()
+    const product = await productApi.getAll<Product[]>();
 
     return (
-        <main className='min-h-screen'>
-            <div className="flex justify-center mt-28">
-                <h1 className="text-4xl font-medium mb-4 h-c">Categorias</h1>
+        <main className="min-h-screen">
+            <div className="flex justify-center mt-28 mb-10">
+                <h1 className="text-4xl font-bold text-zinc-800 tracking-wide">Categorías</h1>
             </div>
-            <div className="
-                grid 
-                justify-items-center 
-                justify-center 
-                lg:[grid-template-columns:repeat(5,160px)]
-                sm:[grid-template-columns:repeat(3,160px)]
-                "
+
+            <div
+                className="
+                grid
+                grid-cols-2
+                sm:grid-cols-3
+                lg:[grid-template-columns:repeat(auto-fit,minmax(180px,1fr))]
+                gap-6
+                justify-center
+                justify-items-center
+                px-6
+            "
             >
                 {categorias.map((cat, i) => (
                     <Link
-                        href={`/product/category/${cat.slug}`}
+                        href={`/category/${cat.categoryId}`}
                         key={i}
-                        className="flex flex-col items-center"
+                        className="group flex flex-col items-center max-sm:last:col-span-2"
                     >
-                        <div className="w-28 h-28 bg-blue-300 shadow-xl/30 rounded-full flex items-center justify-center">
+                        <div
+                            className="
+                w-28 h-28 rounded-full bg-blue-300 shadow-md
+                flex items-center justify-center
+                hover:-translate-y-2 hover:scale-105 hover:shadow-2xl
+                transition-all duration-300 ease-out
+              "
+                        >
                             <Image
                                 src={cat.icono}
                                 alt={cat.nombre}
                                 width={60}
                                 height={60}
-                                className="object-contain"
+                                className="object-contain transition-transform duration-300 group-hover:rotate-6"
                             />
                         </div>
-                        <p className="mt-2 text-lg font-medium">{cat.nombre}</p>
+                        <p
+                            className="
+                mt-3 text-lg font-medium text-gray-700 
+                group-hover:text-blue-600 transition-colors duration-300
+              "
+                        >
+                            {cat.nombre}
+                        </p>
                     </Link>
                 ))}
             </div>
-            <div className="
-                    grid 
-                    justify-center 
-                    gap-7 
-                    mt-20 mb-18
-                    [grid-template-columns:repeat(1,280px)]
-                    sm:[grid-template-columns:repeat(2,280px)]
-                    lg:[grid-template-columns:repeat(3,280px)]
-                ">
+
+            <div
+                className="
+          grid 
+          justify-center 
+          gap-7 
+          mt-20 mb-18
+          [grid-template-columns:repeat(1,280px)]
+          sm:[grid-template-columns:repeat(2,280px)]
+          lg:[grid-template-columns:repeat(3,280px)]
+        "
+            >
                 {product.map((p) => (
                     <ProductGallery
                         key={p._id}
@@ -75,12 +90,11 @@ export default async function productsHome() {
                         productName={p.productName}
                         price={p.price}
                         images={p.images}
+                        color={p.color}
+                        offer={p.offer}
                     />
                 ))}
-
-
             </div>
         </main>
-
     );
 }
